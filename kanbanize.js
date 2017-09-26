5208323eb66e075a09707cc1b49e19338a10bf99
jpherron <jpherron@gmail.com>
2017-09-26 15:22:10 -0400

    dsfgsdfg

--- kanbanize.js
+++ kanbanize.js
@@ -44,9 +44,9 @@ myConnector.getSchema = function (schemaCallback) {
 myConnector.getData = function() {
   $.ajax({
     headers : { 'apikey' : 'EqnM1qQSfWpY7R8RJ76Ufd87ilW5dGReMHnAW1mA' },
-    method: 'POST',
+    method: "POST",
     url: "https://globalnoc.kanbanize.com/index.php/api/kanbanize/get_all_tasks/boardid/2//format/json",
-    dataType: 'jsonp',
+    dataType: 'json',
     success: function(results) {
         console.log(results);
         }
